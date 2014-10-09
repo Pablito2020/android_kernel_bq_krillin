b1e60c84b72ca0a4616ccad07a2c9c9453b1ac31
Oleg Nesterov <oleg@redhat.com>
2014-10-09 15:25:34 -0700

    fs/proc/task_mmu.c: kill the suboptimal and confusing m->version logic
    m_start() carefully documents, checks, and sets "m->version = -1" if
    we are going to return NULL. The only problem is that we will be never
    called again if m_start() returns NULL, so this is simply pointless
    and misleading.
    
    Otoh, ->show() methods m->version = 0 if vma == tail_vma and this is
    just wrong, we want -1 in this case. And in fact we also want -1 if
    ->vm_next == NULL and ->tail_vma == NULL.
    
    And it is not used consistently, the "scan vmas" loop in m_start()
    should update last_addr too.
    
    Finally, imo the whole "last_addr" logic in m_start() looks horrible.
    find_vma(last_addr) is called unconditionally even if we are not going
    to use the result. But the main problem is that this code participates
    in tail_vma-or-NULL mess, and this looks simply unfixable.
    
    Remove this optimization. We will add it back after some cleanups.
    
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Cc: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Cyrill Gorcunov <gorcunov@openvz.org>
    Cc: "Eric W. Biederman" <ebiederm@xmission.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: W4TCH0UT <ateekujjawal@gmail.com>
    Signed-off-by: Anik1199 <anik9280@gmail.com>
    
    Conflicts:
    	fs/proc/task_mmu.c
    
    Conflicts:
    	fs/proc/task_mmu.c

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -188,7 +188,6 @@ static void vma_stop(struct proc_maps_private *priv)
 static void *m_start(struct seq_file *m, loff_t *pos)
 {
 	struct proc_maps_private *priv = m->private;
-	unsigned long last_addr = m->version;
 	struct mm_struct *mm;
 	struct vm_area_struct *vma, *tail_vma = NULL;
 	loff_t l = *pos;
@@ -214,12 +213,6 @@ static void *m_start(struct seq_file *m, loff_t *pos)
 	tail_vma = get_gate_vma(mm);
 	priv->tail_vma = tail_vma;
 	hold_task_mempolicy(priv);
-	/* Start with last addr hint */
-	vma = find_vma(mm, last_addr);
-	if (last_addr && vma) {
-		vma = vma->vm_next;
-		goto out;
-	}
 
 	/*
 	 * Check the vma index is within the range and do
@@ -240,8 +233,6 @@ out:
 	if (vma)
 		return vma;
 
-	/* End of vmas has been reached */
-	m->version = (tail_vma != NULL)? 0: -1UL;
 	if (tail_vma)
 		return tail_vma;
 
@@ -409,14 +400,7 @@ done:
 
 static int show_map(struct seq_file *m, void *v, int is_pid)
 {
-	struct vm_area_struct *vma = v;
-	struct proc_maps_private *priv = m->private;
-
-	show_map_vma(m, vma, is_pid);
-
-	if (m->count < m->size)  /* vma is copied successfully */
-		m->version = (vma != priv->tail_vma)
-			? vma->vm_start : 0;
+	show_map_vma(m, v, is_pid);
 	return 0;
 }
 
@@ -669,7 +653,6 @@ static void show_smap_vma_flags(struct seq_file *m, struct vm_area_struct *vma)
 
 static int show_smap(struct seq_file *m, void *v, int is_pid)
 {
-	struct proc_maps_private *priv = m->private;
 	struct vm_area_struct *vma = v;
 	struct mem_size_stats mss;
 	struct mm_walk smaps_walk = {
@@ -731,9 +714,6 @@ static int show_smap(struct seq_file *m, void *v, int is_pid)
 		seq_putc(m, '\n');
 	}
 
-	if (m->count < m->size)  /* vma is copied successfully */
-		m->version = (vma != priv->tail_vma)
-			? vma->vm_start : 0;
 	return 0;
 }
 
@@ -1444,9 +1424,6 @@ static int show_numa_map(struct seq_file *m, void *v, int is_pid)
 			seq_printf(m, " N%d=%lu", n, md->node[n]);
 out:
 	seq_putc(m, '\n');
-
-	if (m->count < m->size)
-		m->version = (vma != proc_priv->tail_vma) ? vma->vm_start : 0;
 	return 0;
 }
 
