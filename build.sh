822813a8aa77465a64a9ffed9ceb65c5219ec8b5
Pablito2020 <socdelbarca72@gmail.com>
2017-05-01 20:04:52 +0200

    build.sh: Make script beautifull! (add colors)

--- build.sh
+++ build.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-#Set Color
+# Set colors!
 blue='\033[0;34m'
 cyan='\033[0;36m'
 yellow='\033[0;33m'
+green='\033[0;32m'
 red='\033[0;31m'
 nocol='\033[0m'
+orange='\033[0;33m'
+light_red='\033[1;31m'
+purple='\033[0;35m'
 
+
+# Export toolchain + build user and host
 export ARCH=arm CROSS_COMPILE=../arm-eabi-4.8/bin/arm-eabi-
 export KBUILD_BUILD_USER=pablito
 export KBUILD_BUILD_HOST=developer
 
 #For checking errors
-echo "$cyan Remove kernel..."
+echo -e "${cyan} Remove kernel..."
 rm -rf arch/arm/boot/zImage
 
-echo "$cyan Configure krillin.... "
+echo -e "${cyan} Configure krillin.... "
 make alps_defconfig
 
-echo "$cyan Building CM for krillin...."
+echo -e "${purple} Building DareDevil 6.0 for krillin...."
 make zImage
 
+echo -e "${green} Succesfully builded DareDevil Kernel!"
+echo -e $[$SECONDS / 60]' minutes '$[$SECONDS % 60]' seconds'
 
-echo Kernel succesfully builded
-echo "#Pablito2020"
