b9fa9d558ec9a1b8813c3236ba652d9d8945ce37
Pablo Fraile <socdelbarca72@gmail.com>
2017-05-30 12:51:21 +0200

    bootimg.sh: some changes

--- bootimg.sh
+++ bootimg.sh
@@ -4,10 +4,11 @@
 # MOFIFIED BY PABLITO2020 FOR THE 3.10 LINUX KERNEL SOURCE CODE
 PRJ=krillin
  echo " Generating boot.img  ..."
- boot-img/acp -uv arch/arm/boot/kernel_${PRJ}.bin arch/arm/boot/kernel
+ mv arch/arm/boot/Zimage arch/arm/boot kernel
+ boot-img/acp -uv arch/arm/boot/kernel #arch/arm/boot/kernel_${PRJ}.bin
  boot-img/mkbootfs boot-img/host/root | boot-img/minigzip > arch/arm/boot/ramdisk.img
  boot-img/mkimage arch/arm/boot/ramdisk.img ROOTFS > arch/arm/boot/ramdisk_android.img
- mv arch/arm/boot/ramdisk.img boot-img/mediatek/${PRJ}_ramdisk.img
+ # mv arch/arm/boot/ramdisk.img boot-img/mediatek/${PRJ}_ramdisk.img
  # mv arch/arm/boot/ramdisk_android.img out/target/product/${PRJ}/ramdisk.img // Don't know what to do with this for now...
  boot-img/mkbootimg  --kernel arch/arm/boot/kernel --ramdisk arch/arm/boot/ramdisk.img --board 1336460062 --output arch/arm/boot/boot.img
  echo "####################################"
