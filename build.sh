6cfc4e1730a9a399a5bc3b3d8b7d5f7a7b0ef471
Pablito2020 <socdelbarca72@gmail.com>
2017-09-04 16:00:19 +0100

    Daredevil N: Update some little things

--- build.sh
+++ build.sh
@@ -1,28 +1,47 @@
-export ARCH=arm CROSS_COMPILE=../android_toolchain/linaro-4.9/bin/arm-linux-androideabi-
-export KBUILD_BUILD_USER=assusdan
-export KBUILD_BUILD_HOST=SRT
+#!/bin/bash
 
-    #For checking errors
-echo 'Remove kernel...'
-rm -rf arch/arm/boot/zImage
+# SET COLORS! (SOME OF THEM ARE NOT USED, BUT IF YOU LIKE, YOU CAN ADD IT IN THE ECHO LINES)
+blue='\033[0;34m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+green='\033[0;32m'
+red='\033[0;31m'
+nocol='\033[0m'
+orange='\033[0;33m'
+light_red='\033[1;31m'
+purple='\033[0;35m'
 
+# SAY INFO ABOUT THE KERNEL AND THE SCRIPT
+echo -e "${orange}####################################################"
+echo -e "${orange}#      INFO ABOUT THE KERNEL AND THE SCRIPT        #"
+echo -e "${orange}####################################################"
+echo -e "${orange}#                  KERNEL INFO:                    #"
+echo -e "${orange}#             DEVICE: BQ AQUARIS E4.5              #"
+echo -e "${orange}#          LINUX KERNEL VERSION: 3.10.49           #"
+echo -e "${orange}####################################################"
+echo -e "${orange}#                  SCRIPT INFO:                    #"
+echo -e "${orange}#             DEVELOPER: PABLITO2020               #"
+echo -e "${orange}####################################################"
 
-echo 'Configure CM Zera S '
-make alps_defconfig >/dev/null
+# EXPORT TOOLCHAIN
+export ARCH=arm CROSS_COMPILE=../arm-eabi-4.8/bin/arm-eabi-
 
-echo 'Building CM Zera S'
-make -j4 zImage >/dev/null 2>buildlog.log
+# User and Build Host
+export KBUILD_BUILD_USER=Pablito2020
+export KBUILD_BUILD_HOST=Daredevil
 
-    #check errors
-if [ ! -f arch/arm/boot/zImage ]
-then
-    echo "BUID ERRORS!"
-else
- #if OK
-echo 'Moving CM Zera S'
-mv arch/arm/boot/zImage /var/www/compiled/CM-zImage-zeras
-fi
+# READ THE Lineage/AOSP DEFCONFIG
+echo -e "${cyan} CONFIGURE KRILLIN.."
+make lineage_krillin_defconfig
 
+# BUILD ZIMAGE
+echo -e "${purple} BUILD DAREDEVIL KERNEL FOR KRILLIN.."
+make zImage
 
-#write worktime
-echo $[$SECONDS / 60]' minutes '$[$SECONDS % 60]' seconds' 
+# SAY YES TO A SUCCESFULLY BUILD :)
+echo -e "${green} SUCCESSFULLY BUILDED DAREDEVIL KERNEL"
+echo -e "${green} ZIMAGE IS IN ARCH/ARM/BOOT/ZIMAGE"
+echo -e "${green} MAKE BOOT.IMG WITH CARLIV IMAGE KITCHEN"
+echo -e "${green} HAVE FUN AND GIVE PROPER CREDITS!"
+echo -e $[$SECONDS / 60]' minutes '$[$SECONDS % 60]' seconds'
+
