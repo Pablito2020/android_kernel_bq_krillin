92bd73a082b4ec7e987007318f9c18d93862d2f4
Pablito2020 <socdelbarca72@gmail.com>
2017-09-28 13:28:21 +0200

    Revert "printk: do cond_resched() between lines while outputting to consoles"
    This reverts commit 765a33c11e3efd255f0f1708db1b612eba06f12d.

--- kernel/panic.c
+++ kernel/panic.c
@@ -22,7 +22,6 @@
 #include <linux/sysrq.h>
 #include <linux/init.h>
 #include <linux/nmi.h>
-#include <linux/console.h>
 
 #define PANIC_TIMER_STEP 100
 #define PANIC_BLINK_SPD 18
@@ -134,8 +133,6 @@ void panic(const char *fmt, ...)
 	atomic_notifier_call_chain(&panic_notifier_list, 0, buf);
 
 	bust_spinlocks(0);
-
-	console_flush_on_panic();
 
 	if (!panic_blink)
 		panic_blink = no_blink;
