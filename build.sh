8a714273adf77dba5c247ff9ca335c74c1685e71
Pablito2020 <socdelbarca72@gmail.com>
2017-09-29 12:22:33 +0200

    pablito-scripts: add gnu and copyright
    For prevent kangers? And have the gnu license

--- build.sh
+++ build.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+#
+# Daredevil Kernel compilation script
+#
+# Copyright (C) 2017 Pablo Fraile Alonso (Pablito2020)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
 
 # Set Colors! (some of there aren't used, but if you like, you can add it in the echo lines.)
 blue='\033[0;34m'
