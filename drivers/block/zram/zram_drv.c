c15f62070b90b36bd89088993eae65db504953c3
Pablito2020 <socdelbarca72@gmail.com>
2017-10-11 20:01:36 +0200

    Revert "zram: don't call idr_remove() from zram_remove()"
    This reverts commit 8890f4ed8b7fe91386d812ec9c048193e1467279.

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1276,6 +1276,7 @@ static int zram_remove(struct zram *zram)
 
 	pr_info("Removed device: %s\n", zram->disk->disk_name);
 
+	idr_remove(&zram_index_idr, zram->disk->first_minor);
 	blk_cleanup_queue(zram->disk->queue);
 	del_gendisk(zram->disk);
 	put_disk(zram->disk);
@@ -1317,12 +1318,10 @@ static ssize_t hot_remove_store(struct class *class,
 	mutex_lock(&zram_index_mutex);
 
 	zram = idr_find(&zram_index_idr, dev_id);
-	if (zram) {
+	if (zram)
 		ret = zram_remove(zram);
-		idr_remove(&zram_index_idr, dev_id);
-	} else {
+	else
 		ret = -ENODEV;
-	}
 
 	mutex_unlock(&zram_index_mutex);
 	return ret ? ret : count;
