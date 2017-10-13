fe3428ce077f96a5e4646c0221fcf47e04b38ea2
Pablito2020 <socdelbarca72@gmail.com>
2017-10-13 15:47:52 +0200

    Revert "fs/proc/task_mmu.c: cleanup the "tail_vma" horror in m_next()"
    This reverts commit fd5dba54bc42c8db53a2c40dce16e4a32d88429c.

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -257,13 +257,15 @@ out:
 static void *m_next(struct seq_file *m, void *v, loff_t *pos)
 {
 	struct proc_maps_private *priv = m->private;
+	struct vm_area_struct *vma = v;
 	struct vm_area_struct *tail_vma = priv->tail_vma;
-	struct vm_area_struct *vma = v, *next = NULL;
+	struct vm_area_struct *next;
 
 	(*pos)++;
-	if (vma != tail_vma)
-		next = vma->vm_next ?: tail_vma;
+	if (vma && (vma != tail_vma) && vma->vm_next)
+		return vma->vm_next;
 
+	next = (vma != tail_vma) ? tail_vma : NULL;
 	if (!next)
 		vma_stop(priv);
 	return next;
